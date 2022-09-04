13fe3d10fe75e88d09146a24af0ffd7009ef7fbe
Domotor Zsolti <d.zsolti@yahoo.com>
2022-09-04 16:23:27 +0300

    feat(common styles): refactor and added 0 margins and padding

--- src/utils/commonStyles.tsx
+++ src/utils/commonStyles.tsx
@@ -2,7 +2,131 @@ import { StyleSheet, Dimensions } from "react-native";
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get("screen");
 
-export const commonStyles = StyleSheet.create({
+const margins = () => {
+    let x = {
+        margin0: { margin: 0 },
+        margin1: { margin: 1 },
+        margin2: { margin: 2 },
+        margin3: { margin: 4 },
+        margin4: { margin: 8 },
+        margin5: { margin: 16 },
+        margin6: { margin: 32 },
+
+        marginLeft0: { marginLeft: 0 },
+        marginLeft1: { marginLeft: 1 },
+        marginLeft2: { marginLeft: 2 },
+        marginLeft3: { marginLeft: 4 },
+        marginLeft4: { marginLeft: 8 },
+        marginLeft5: { marginLeft: 16 },
+        marginLeft6: { marginLeft: 32 },
+
+        marginRight0: { marginRight: 0 },
+        marginRight1: { marginRight: 1 },
+        marginRight2: { marginRight: 2 },
+        marginRight3: { marginRight: 4 },
+        marginRight4: { marginRight: 8 },
+        marginRight5: { marginRight: 16 },
+        marginRight6: { marginRight: 32 },
+
+        marginTop0: { marginTop: 0 },
+        marginTop1: { marginTop: 1 },
+        marginTop2: { marginTop: 2 },
+        marginTop3: { marginTop: 4 },
+        marginTop4: { marginTop: 8 },
+        marginTop5: { marginTop: 16 },
+        marginTop6: { marginTop: 32 },
+
+        marginBottom0: { marginBottom: 0 },
+        marginBottom1: { marginBottom: 1 },
+        marginBottom2: { marginBottom: 2 },
+        marginBottom3: { marginBottom: 4 },
+        marginBottom4: { marginBottom: 8 },
+        marginBottom5: { marginBottom: 16 },
+        marginBottom6: { marginBottom: 32 },
+
+        marginVertical0: { marginVertical: 0 },
+        marginVertical1: { marginVertical: 1 },
+        marginVertical2: { marginVertical: 2 },
+        marginVertical3: { marginVertical: 4 },
+        marginVertical4: { marginVertical: 8 },
+        marginVertical5: { marginVertical: 16 },
+        marginVertical6: { marginVertical: 32 },
+
+        marginHorizontal0: { marginHorizontal: 0 },
+        marginHorizontal1: { marginHorizontal: 1 },
+        marginHorizontal2: { marginHorizontal: 2 },
+        marginHorizontal3: { marginHorizontal: 4 },
+        marginHorizontal4: { marginHorizontal: 8 },
+        marginHorizontal5: { marginHorizontal: 16 },
+        marginHorizontal6: { marginHorizontal: 32 },
+    };
+
+    return x;
+};
+
+const paddings = () => {
+    let x = {
+        padding0: { padding: 0 },
+        padding1: { padding: 1 },
+        padding2: { padding: 2 },
+        padding3: { padding: 4 },
+        padding4: { padding: 8 },
+        padding5: { padding: 16 },
+        padding6: { padding: 32 },
+
+        paddingLeft0: { paddingLeft: 0 },
+        paddingLeft1: { paddingLeft: 1 },
+        paddingLeft2: { paddingLeft: 2 },
+        paddingLeft3: { paddingLeft: 4 },
+        paddingLeft4: { paddingLeft: 8 },
+        paddingLeft5: { paddingLeft: 16 },
+        paddingLeft6: { paddingLeft: 32 },
+
+        paddingRight0: { paddingRight: 0 },
+        paddingRight1: { paddingRight: 1 },
+        paddingRight2: { paddingRight: 2 },
+        paddingRight3: { paddingRight: 4 },
+        paddingRight4: { paddingRight: 8 },
+        paddingRight5: { paddingRight: 16 },
+        paddingRight6: { paddingRight: 32 },
+
+        paddingTop0: { paddingTop: 0 },
+        paddingTop1: { paddingTop: 1 },
+        paddingTop2: { paddingTop: 2 },
+        paddingTop3: { paddingTop: 4 },
+        paddingTop4: { paddingTop: 8 },
+        paddingTop5: { paddingTop: 16 },
+        paddingTop6: { paddingTop: 32 },
+
+        paddingBottom0: { paddingBottom: 0 },
+        paddingBottom1: { paddingBottom: 1 },
+        paddingBottom2: { paddingBottom: 2 },
+        paddingBottom3: { paddingBottom: 4 },
+        paddingBottom4: { paddingBottom: 8 },
+        paddingBottom5: { paddingBottom: 16 },
+        paddingBottom6: { paddingBottom: 32 },
+
+        paddingVertical0: { paddingVertical: 0 },
+        paddingVertical1: { paddingVertical: 1 },
+        paddingVertical2: { paddingVertical: 2 },
+        paddingVertical3: { paddingVertical: 4 },
+        paddingVertical4: { paddingVertical: 8 },
+        paddingVertical5: { paddingVertical: 16 },
+        paddingVertical6: { paddingVertical: 32 },
+
+        paddingHorizontal0: { paddingHorizontal: 0 },
+        paddingHorizontal1: { paddingHorizontal: 1 },
+        paddingHorizontal2: { paddingHorizontal: 2 },
+        paddingHorizontal3: { paddingHorizontal: 4 },
+        paddingHorizontal4: { paddingHorizontal: 8 },
+        paddingHorizontal5: { paddingHorizontal: 16 },
+        paddingHorizontal6: { paddingHorizontal: 32 },
+    };
+
+    return x;
+};
+
+const styles = StyleSheet.create({
     fill: {
         flex: 1,
     },
@@ -91,114 +215,19 @@ export const commonStyles = StyleSheet.create({
     absolute: { position: "absolute" },
     absoluteBottom: { position: "absolute", bottom: 0, left: 0, right: 0 },
     absoluteTop: { position: "absolute", top: 0, left: 0, right: 0 },
+    absoluteFill: StyleSheet.absoluteFillObject,
 
-    //#region margins
-
-    margin1: { margin: 1 },
-    margin2: { margin: 2 },
-    margin3: { margin: 4 },
-    margin4: { margin: 8 },
-    margin5: { margin: 16 },
-    margin6: { margin: 32 },
-
-    marginLeft1: { marginLeft: 1 },
-    marginLeft2: { marginLeft: 2 },
-    marginLeft3: { marginLeft: 4 },
-    marginLeft4: { marginLeft: 8 },
-    marginLeft5: { marginLeft: 16 },
-    marginLeft6: { marginLeft: 32 },
-
-    marginRight1: { marginRight: 1 },
-    marginRight2: { marginRight: 2 },
-    marginRight3: { marginRight: 4 },
-    marginRight4: { marginRight: 8 },
-    marginRight5: { marginRight: 16 },
-    marginRight6: { marginRight: 32 },
-
-    marginTop1: { marginTop: 1 },
-    marginTop2: { marginTop: 2 },
-    marginTop3: { marginTop: 4 },
-    marginTop4: { marginTop: 8 },
-    marginTop5: { marginTop: 16 },
-    marginTop6: { marginTop: 32 },
-
-    marginBottom1: { marginBottom: 1 },
-    marginBottom2: { marginBottom: 2 },
-    marginBottom3: { marginBottom: 4 },
-    marginBottom4: { marginBottom: 8 },
-    marginBottom5: { marginBottom: 16 },
-    marginBottom6: { marginBottom: 32 },
-
-    marginVertical1: { marginVertical: 1 },
-    marginVertical2: { marginVertical: 2 },
-    marginVertical3: { marginVertical: 4 },
-    marginVertical4: { marginVertical: 8 },
-    marginVertical5: { marginVertical: 16 },
-    marginVertical6: { marginVertical: 32 },
-
-    marginHorizontal1: { marginHorizontal: 1 },
-    marginHorizontal2: { marginHorizontal: 2 },
-    marginHorizontal3: { marginHorizontal: 4 },
-    marginHorizontal4: { marginHorizontal: 8 },
-    marginHorizontal5: { marginHorizontal: 16 },
-    marginHorizontal6: { marginHorizontal: 32 },
-
-    //#endregion
-
-    //#region paddings
-
-    padding1: { padding: 1 },
-    padding2: { padding: 2 },
-    padding3: { padding: 4 },
-    padding4: { padding: 8 },
-    padding5: { padding: 16 },
-    padding6: { padding: 32 },
-
-    paddingLeft1: { paddingLeft: 1 },
-    paddingLeft2: { paddingLeft: 2 },
-    paddingLeft3: { paddingLeft: 4 },
-    paddingLeft4: { paddingLeft: 8 },
-    paddingLeft5: { paddingLeft: 16 },
-    paddingLeft6: { paddingLeft: 32 },
-
-    paddingRight1: { paddingRight: 1 },
-    paddingRight2: { paddingRight: 2 },
-    paddingRight3: { paddingRight: 4 },
-    paddingRight4: { paddingRight: 8 },
-    paddingRight5: { paddingRight: 16 },
-    paddingRight6: { paddingRight: 32 },
-
-    paddingTop1: { paddingTop: 1 },
-    paddingTop2: { paddingTop: 2 },
-    paddingTop3: { paddingTop: 4 },
-    paddingTop4: { paddingTop: 8 },
-    paddingTop5: { paddingTop: 16 },
-    paddingTop6: { paddingTop: 32 },
-
-    paddingBottom1: { paddingBottom: 1 },
-    paddingBottom2: { paddingBottom: 2 },
-    paddingBottom3: { paddingBottom: 4 },
-    paddingBottom4: { paddingBottom: 8 },
-    paddingBottom5: { paddingBottom: 16 },
-    paddingBottom6: { paddingBottom: 32 },
-
-    paddingVertical1: { paddingVertical: 1 },
-    paddingVertical2: { paddingVertical: 2 },
-    paddingVertical3: { paddingVertical: 4 },
-    paddingVertical4: { paddingVertical: 8 },
-    paddingVertical5: { paddingVertical: 16 },
-    paddingVertical6: { paddingVertical: 32 },
-
-    paddingHorizontal1: { paddingHorizontal: 1 },
-    paddingHorizontal2: { paddingHorizontal: 2 },
-    paddingHorizontal3: { paddingHorizontal: 4 },
-    paddingHorizontal4: { paddingHorizontal: 8 },
-    paddingHorizontal5: { paddingHorizontal: 16 },
-    paddingHorizontal6: { paddingHorizontal: 32 },
-
-    //#endregion
+    ...margins(),
+    ...paddings(),
 });
-(commonStyles as any).square = (x: number) => ({
-    width: x,
-    height: x,
-});
+
+const functions = {
+    square: (x: number) => ({
+        width: x,
+        height: x,
+    }),
+};
+
+type Prop = typeof styles & typeof functions;
+
+export const commonStyles: Prop = Object.setPrototypeOf(styles, functions);
