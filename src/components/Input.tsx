6d64a5dddc5429267f565b98a308c0c7a9933321
Domotor Zsolti <d.zsolti@yahoo.com>
2022-10-04 20:14:01 +0300

    feat(input component): added ref

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { Text, TextInput, TextInputProps } from "react-native";
 import { commonStyles } from "../utils/commonStyles";
 import { colors } from "../utils/colors";
@@ -6,43 +6,40 @@ import { colors } from "../utils/colors";
 interface Props extends TextInputProps {
     value: string;
     setValue: (newValue: string) => void;
+    ref?: any;
     error?: string;
     onSubmit: () => void;
 }
-const Input: React.FC<Props> = ({
-    value,
-    setValue,
-    onSubmit,
-    error,
-    style,
-    ...props
-}) => {
-    return (
-        <>
-            <TextInput
-                {...props}
-                value={value}
-                onChangeText={setValue}
-                onSubmitEditing={onSubmit}
-                style={[
-                    commonStyles.padding4,
-                    commonStyles.paddingHorizontal5,
-                    commonStyles.marginBottom4,
-                    commonStyles.roundedSmall,
-                    { width: "100%" } /* todo this in commonStyles */,
-                    {
-                        color: colors.black,
-                        backgroundColor: colors.gray,
-                        borderWidth: 1,
-                        borderColor: colors.darkerGray,
-                    },
-                    style,
-                ]}
-            />
-            {error ? (
-                <Text style={[{ color: colors.danger }]}>{error}</Text>
-            ) : null}
-        </>
-    );
-};
+const Input: React.FC<Props> = forwardRef(
+    ({ value, setValue, onSubmit, error, style, ...props }, ref: any) => {
+        return (
+            <>
+                <TextInput
+                    {...props}
+                    ref={ref}
+                    value={value}
+                    onChangeText={setValue}
+                    onSubmitEditing={onSubmit}
+                    style={[
+                        commonStyles.padding4,
+                        commonStyles.paddingHorizontal5,
+                        commonStyles.marginBottom4,
+                        commonStyles.roundedSmall,
+                        { width: "100%" } /* todo this in commonStyles */,
+                        {
+                            color: colors.black,
+                            backgroundColor: colors.gray,
+                            borderWidth: 1,
+                            borderColor: colors.darkerGray,
+                        },
+                        style,
+                    ]}
+                />
+                {error ? (
+                    <Text style={[{ color: colors.danger }]}>{error}</Text>
+                ) : null}
+            </>
+        );
+    }
+);
 export default Input;
