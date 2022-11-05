09a6f47c70a74c1fd7104587f05e38c2c6a38a8d
Domotor Zsolti <d.zsolti@yahoo.com>
2022-11-05 14:54:45 +0200

    fix(lint): end of line and translation adjustments

--- src/services/StorageService.ts
+++ src/services/StorageService.ts
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export const STORAGE_KEYS = {
   INTERESTS: "INTERESTS",
   COLLECTIONS: "COLLECTIONS",
-  LANGUAGE: "LANGUAGE"
+  LANGUAGE: "LANGUAGE",
 };
 
 class StorageService {
